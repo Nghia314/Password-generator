dc39e56cb1e0a4369ad7260922ad9bbd497d960c
Nghia Le <davidle314@hotmail.com>
2022-03-30 09:35:11 -0700

    adding pwcriteria

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// Password global scope
+var criteria = {
+uppercase: [ABCDEFGHIJKLMNOPQRSTUVWXYZ]
+lowercase: [abcdefghijklmnopqrdtuvwxyz]
+number : [0123456789]
+specialchara: [! , @ , # , $ , % , ^ , & , * , ( , ) , + , = ]
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
