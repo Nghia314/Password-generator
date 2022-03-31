b3aed4316f5d502d87cb0b3fce65f2b0d5f81e9c
Nghia Le <davidle314@hotmail.com>
2022-03-30 21:22:16 -0700

    redo the project and do the psuedocode

--- develop/script.js
+++ develop/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+// creating password criteria 
 var passwordcriteria = {
    Uppercase: ["ABCDFGHJIKMNLOPQRSTUVWXYZ"],
    lowercase: ["abcdefghiklmnopqrstuvwxyz"],
@@ -17,21 +17,43 @@ function writePassword() {
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+//function the generate button to generate password
 function generatePassword() {
-  console.log("clicked the button");
-  
+  //
+  var result ="";
+
   var length = 0;
   var Uppercase;
   var lowercase;
   var Number;
-  var specialchara
+  var specialchara;
 
-  var result = "";
+  length = 0;
+  passwordcriteria.length= 0;
+  result="";
 
-  if((length < 8 || length >128))
-  alert("Choose number between 8 and 128")
+  while (length < 8 || length > 128) {
+    length = prompt("How many characters do you want to be in your password? at least 8 and no more than 128.");
 
-return "passwordgenerate"
+    if (length == null){
+      return "your secure password";
+    }
+    else {
+    if (undefined(length)) {
+      alert ("you did not enter a number");
+      return "No Password was generate"
+    }
+    }
+    else {
+      if ( length < 8 || length > 128){
+        alert("password must be between 8 and 128 character.");
+        return "Your secure password";
+      }
+    }
+    else{
+      
+    }
+  }
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
